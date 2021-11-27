c7d0e204b054b31e84d50537be605129e5a087bb
hamiltondowdy <hddowdy@ncsu.edu>
2021-11-27 11:01:24 -0600

    added random value generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,9 @@ THEN the password is either displayed in an alert or written to the page */
 
 // Write function to generate password
 const characterCats = {
-specialArr: ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', ']', '[', '\', ^', '_', '`', '{', '|', '}', '~'],
-lowercaseArr: ['z', 'a', 'q', 'x', 's', 'w', 'c', 'd', 'e', 'v', 'f', 'r', 'b', 'g', 't', 'n', 'h', 'y', 'm', 'j', 'u', 'k', 'i', 'l', 'o', 'p'],
-uppercaseArr: ['L', 'K', 'J', 'H', 'G', 'F', 'D', 'S', 'A', 'M', 'N', 'B', 'V', 'C', 'X', 'Z', 'P', 'O', 'I', 'U', 'Y', 'T', 'R', 'E', 'W', 'Q'],
+specialArr:['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', ']', '[', '\', ^', '_', '`', '{', '|', '}', '~'],
+lowercaseArr:['z', 'a', 'q', 'x', 's', 'w', 'c', 'd', 'e', 'v', 'f', 'r', 'b', 'g', 't', 'n', 'h', 'y', 'm', 'j', 'u', 'k', 'i', 'l', 'o', 'p'],
+uppercaseArr:['L', 'K', 'J', 'H', 'G', 'F', 'D', 'S', 'A', 'M', 'N', 'B', 'V', 'C', 'X', 'Z', 'P', 'O', 'I', 'U', 'Y', 'T', 'R', 'E', 'W', 'Q'],
 numericArr: ['0', '9', '7', '3', '1', '6', '8', '4', '2', '5'],
 }
 
@@ -30,19 +30,19 @@ function passwordLength () {
     alert("Hey buddy, between 8 and 128 characters. You know the rules!")
     passwordLength();
   } else {
-    return passLength
+    return passLength;
   }
-};
+}
 
-function chooseCharTypes () {
+function chooseCharTypes() { 
   let viableChars = ''
 
-  let upperCase = confirm('Upper Case Letters? (A-Z)')
-  let lowerCase = confirm('Lower Case Letters? (a-z)')
-  let numberCase = confirm('Do you want numbers? (0-9')
-  let specialCase = confirm('Do you want symbols? (#$^&@..)')
+  let upperCase = confirm('Include Uppercase Letters? (A-Z)')
+  let lowerCase = confirm('Include Lowercase Letters? (a-z)') 
+  let numberCase = confirm('Include Numbers? "0-9"')
+  let specialCase = confirm('Include Special Characters? "*+[]@#^..')
 
-  if (!upperCase && !lowerCase && !numberCase && !specialCase) {
+if (!upperCase && !lowerCase && !numberCase && !specialCase) {
     alert("How are you gonna have a password with no characters? Try again.");
     chooseCharTypes();
   } else {
@@ -59,6 +59,7 @@ function chooseCharTypes () {
       viableChars += characterCats.specialArr.join('')
     }
 
+console.log(upperCase, lowerCase, numberCase, specialCase);
 
 return viableChars;
 
@@ -70,8 +71,8 @@ function generatePassword() {
   let desiredLength = passwordLength();
   let chosenChars = chooseCharTypes();
 
-  for (var i=0; i < desiredLength; i++) {
-    password += chosenChars
+  for (var i = 0; i < desiredLength; i++) {
+    password += chosenChars[(Math.floor(Math.random() * chosenChars.length))]
     console.log(password);
   }
 return password;
